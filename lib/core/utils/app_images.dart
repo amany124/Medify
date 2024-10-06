c9eb0bc6b1a1d37505f433c57ed219c64303d741
amany <amany@gmail.com>
2024-10-06 16:03:46 +0300

    first commit

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppLogo
+  /// assets/images/app_logo.svg
+  static const String assetsImagesAppLogo = "assets/images/app_logo.svg";
+
+  /// Assets for assetsImagesDoctor
+  /// assets/images/doctor.png
+  static const String assetsImagesDoctor = "assets/images/doctor.png";
+
+  /// Assets for assetsImagesSplashBackground
+  /// assets/images/splash_background.png
+  static const String assetsImagesSplashBackground = "assets/images/splash_background.png";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/Vector.png
+  static const String assetsImagesVector = "assets/images/Vector.png";
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'package:medify/onboarding_screen/ui/views/onboarding_screen.dart';
+import 'package:medify/start_screen/ui/views/start_view.dart';
+import 'splash/ui/views/splash_view.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const gradeApp(),
+    ),
+  );
+}
+
+class gradeApp extends StatelessWidget {
+  const gradeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: //SplashView(),
+        //onboardingView(),
+      StartView(),
+    );
+  }
+}
