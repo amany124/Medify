9d89fa14d4cdfabda02c5eb72476b347489333d7
Abdelmaboud <mohamedabdlmabod03@gmail.com>
2025-07-10 09:43:14 +0300

    feat: Add logging for ChatCubit initialization in AllChats widget

--- lib/features/chat/ui/views/all_chats.dart
+++ lib/features/chat/ui/views/all_chats.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -23,6 +24,7 @@ class _AllChatsState extends State<AllChats> {
   void initState() {
     super.initState();
     _chatCubit = getIt<ChatCubit>();
+    log('AllChats initialized with ChatCubit: $_chatCubit');
     _loadConversations();
   }
 
